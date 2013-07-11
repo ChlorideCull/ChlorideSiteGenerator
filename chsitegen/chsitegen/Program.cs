086746f8eb8e6cf4978012ce8d4aa678aad20abb
Chloride Cull <steamruler+ChlorideCull@gmail.com>
2013-07-11 04:55:04 +0200

    No tears, only sleep now
    Base done, capable of generation but no detection of buttons yet. Do not
    regard as stable.

--- chsitegen/chsitegen/Program.cs
+++ chsitegen/chsitegen/Program.cs
@@ -8,20 +8,25 @@ namespace chsitegen
 {
     class MainClass
     {
-        private bool settingsCreated;
         public static void Main(string[] args)
         {
             Console.WriteLine("Chloride Site Generator");
             Console.WriteLine();
             Console.Write("Checking for site configuration...");
             XmlDocument confd = new XmlDocument();
+            confd.InnerXml = "<SiteSettings></SiteSettings>";
+            MarkdownOptions mdo = new MarkdownOptions();
+            mdo.AutoHyperlink = true;
+            mdo.AutoNewlines = true;
+            mdo.EncodeProblemUrlCharacters = true;
+            mdo.LinkEmails = true;
+            Markdown md = new Markdown(mdo);
 
             if (!File.Exists("site.xml"))
             {
                 Console.WriteLine(" Nonexistant");
                 Console.WriteLine();
                 StreamWriter confsw = File.CreateText("site.xml");
-                settingsCreated = false;
                 Console.Write("Friendly site name: ");
                 AddXmlNode("SiteName", Console.ReadLine(), confd.DocumentElement);
                 Console.Write("Website footer: ");
@@ -33,6 +38,7 @@ namespace chsitegen
                 }
                 Console.Write("Select a theme: ");
                 AddXmlNode("SiteTheme", Console.ReadLine(), confd.DocumentElement);
+                confd.Save(confsw.BaseStream);
             }
             else
             {
@@ -41,21 +47,55 @@ namespace chsitegen
                 confd.LoadXml(confsr.ReadToEnd());
             }
 
-            SortedDictionary<int, KeyValuePair<string, string>> pages = new Dictionary<int, KeyValuePair<string, string>>(); //Unconventional use but meh
+            SortedDictionary<int, string[]> pages = new SortedDictionary<int, string[]>(); //Unconventional use but meh
             Console.Write("Counting pages...");
             foreach (string f in Directory.GetFiles("pages"))
             {
                 string file = Path.GetFileNameWithoutExtension(f);
                 int sortnum = Convert.ToInt32(file.Substring(0, 3));
-                KeyValuePair<string, string> extendedkvp = new KeyValuePair<string, string>();
-                extendedkvp.Key = file.Substring(3);
-                extendedkvp.Value = extendedkvp.Key.ToLowerInvariant().Replace(" ","") + ".html";
-                pages.Add(sortnum, extendedkvp);
+                string[] values = new string[] { file.Substring(3), file.Substring(3).ToLowerInvariant().Replace(" ","") + ".html", f };
+                pages.Add(sortnum, values);
             }
             Console.WriteLine(" " + pages.Count + " pages found");
+            Console.Write("Generating pages...");
+            if (Directory.Exists("html"))
+            {
+                Directory.Delete("html", true);
+            }
+            Directory.CreateDirectory("html");
+
+            string SiteTitle = confd.DocumentElement["SiteName"].InnerText;
+            Console.WriteLine(SiteTitle);
+            string SiteFooter = confd.DocumentElement["SiteFooter"].InnerText;
+            Console.WriteLine(SiteFooter);
+            string SiteTheme = confd.DocumentElement["SiteTheme"].InnerText;
+            Console.WriteLine(SiteTheme);
+            string TemplateHTML = "";
+
+            foreach (string f in Directory.GetFiles("themes/" + SiteTheme + "/"))
+            {
+                if (Path.GetFileName(f) == "index.html")
+                {
+                    TemplateHTML = File.OpenText(f).ReadToEnd();
+                }
+                else
+                {
+                    File.Copy(f, "html/" + Path.GetFileName(f), true);
+                }
+                Console.ReadKey();
+            }
+
+            foreach (KeyValuePair<int, string[]> kvp in pages)
+            {
+                string TemporaryHTML = TemplateHTML;
+                string csginfo = "Generated with Chloride Page Generator at " + DateTime.UtcNow.ToShortTimeString() + ", " + DateTime.UtcNow.ToShortDateString() + " (UTC)";
+                TemporaryHTML = TemporaryHTML.Replace("$WEBSITE_TITLE", SiteTitle).Replace("$WEBSITE_FOOTER", SiteFooter).Replace("$PAGE_URL", kvp.Value[1]).Replace("$PAGE_TITLE", kvp.Value[0]).Replace("$CSG_INFO", csginfo);
+                TemporaryHTML = TemporaryHTML.Replace("$PAGE_CONTENT", md.Transform(File.OpenText(kvp.Value[2]).ReadToEnd()));
+                File.WriteAllText("html/" + kvp.Value[1], TemporaryHTML);
+            }
         }
 
-        private void AddXmlNode(string name, string content, XmlElement parentelem)
+        private static void AddXmlNode(string name, string content, XmlElement parentelem)
         {
             XmlNode xmn = parentelem.OwnerDocument.CreateNode(XmlNodeType.Element, name, "");
             xmn.InnerText = content;
